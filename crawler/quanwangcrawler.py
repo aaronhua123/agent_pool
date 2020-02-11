4fec3deac0643d91a297040963b09f4c4e96e8a4
aaronhua <cjhdwyyx@163.com>
2020-02-11 10:26:51 +0800

    1

--- crawler/quanwangcrawler.py
+++ crawler/quanwangcrawler.py
@@ -1,57 +1,47 @@
 import time
 import requests
 from lxml import etree
-import dbmodle
+import pymysql
 
 def getquanwang():
     url = 'http://www.goubanjia.com/'
     headers = {
         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/69.0.3497.100 Safari/537.36'
     }
-    r = requests.get('http://127.0.0.1:5000/http', timeout=30)
-    proxies = r.json()
-    print(proxies)
     r = requests.get(url, headers=headers, timeout=30)
     return r
 
 def analyzequanwang(r):
     tree = etree.HTML(r.text)
     tr_list = tree.xpath('//table/tbody/tr')
-    print(tr_list)
     _ = []
     for tr in tr_list:
         host_list = tr.xpath('./td[1]/*[not(contains(@style,"none"))]/text()')[:-1]
         host = "".join(host_list)
-        # print(tr.xpath('./td[1]/*[last()]/@class'),tr.xpath('./td[1]/*[last()]/text()'))
         portnum = tr.xpath('./td[1]/*[last()]/@class')[0].split(' ')[1]
-        # print(portnum)
         portlist = [str('ABCDEFGHIZ'.index(port)) for port in portnum]
-        # print(_)
         result = int("".join(portlist))
         port = result >> 3
         protocol = tr.xpath('./td[3]/a/text()')[0]
-        print(protocol, host, port)
-        _.append({
-            protocol: f'{protocol}://{host}:{port}'
-        })
+        _.append((f'{protocol}://{host.strip()}:{port}',protocol,))
     return _
+
 if __name__ == '__main__':
+    conn = pymysql.connect(user='root',password='root',host='127.0.0.1',port=3306,db='proxy')
+    sql = 'insert into proxy.proxy_cache (proxy,protocol,modify_date) values(%s,%s,NOW())'
     while 1:
-        try:
-            r = getquanwang()
-            result = analyzequanwang(r)
-            print('result', result)
-            for proxy in result:
-                for protocol, url in proxy.items():
-                    print("save ", proxy)
-                    if 'https' in url:
-                        dbmodle.sethttps(url)
-                    else:
-                        dbmodle.sethttp(url)
-        except Exception as e:
-            print(e)
-            # raise
-        time.sleep(30)
+        r = getquanwang()
+        result = analyzequanwang(r)
+        for proxy,protocol in result:
+            print(proxy,protocol)
+            try:
+                with conn.cursor() as cur:
+                    cur.execute(sql,args=(proxy,protocol,))
+                conn.commit()
+            except Exception as e:
+                raise
+        time.sleep(60)
+
 
 
 
